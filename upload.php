4b24f3d899d64035e5a646da0b9ab40828931a4f
poxen8 <htmelm@gmail.com>
2016-04-25 21:13:16 +0200

    xml parse

--- upload.php
+++ upload.php
@@ -1,49 +1,100 @@
-<?php
+ï»¿<html>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
 
-if(isset($_POST['submit']))
-{
+    if(isset($_POST['submit']))
+    {
 
-	$uploaddir = 'uploaded/';
-	$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+    	$uploaddir = 'uploaded/';
+    	$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-	echo '<pre>';
-	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) 
-	{
-		echo "Le fichier a été téléchargé avec succès";
-	} 
-	else 
-	{
-		switch ($_FILES['userfile']['error']) 
-		{ 
-			case UPLOAD_ERR_INI_SIZE: 
-				$message = "Le fichié téléchargé dépasse la directive upload_max_filesize configuré dans php.ini"; 
-				break; 
-			case UPLOAD_ERR_FORM_SIZE: 
-				$message = "Le fichié téléchargé dépasse la directive MAX_FILE_SIZE spécifié dans le formulaire HTML"; 
-				break; 
-			case UPLOAD_ERR_PARTIAL: 
-				$message = "Le fichier n'a pas été totalement téléchargé"; 
-				break; 
-			case UPLOAD_ERR_NO_FILE: 
-				$message = "Aucun fichier spécifié"; 
-				break; 
-			case UPLOAD_ERR_NO_TMP_DIR: 
-				$message = "Dossier temporaire introuvable"; 
-				break; 
-			case UPLOAD_ERR_CANT_WRITE: 
-				$message = "Eched d'écriture du fichier sur le disque"; 
-				break; 
+    	echo '<pre>';
+    	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
+    	{
+    		echo "Le fichier a ï¿½tï¿½ tï¿½lï¿½chargï¿½ avec succï¿½s";
+    	}
+    	else
+    	{
+    		switch ($_FILES['userfile']['error'])
+    		{
+    			case UPLOAD_ERR_INI_SIZE:
+    				$message = "Le fichiï¿½ tï¿½lï¿½chargï¿½ dï¿½passe la directive upload_max_filesize configurï¿½ dans php.ini";
+    				break;
+    			case UPLOAD_ERR_FORM_SIZE:
+    				$message = "Le fichiï¿½ tï¿½lï¿½chargï¿½ dï¿½passe la directive MAX_FILE_SIZE spï¿½cifiï¿½ dans le formulaire HTML";
+    				break;
+    			case UPLOAD_ERR_PARTIAL:
+    				$message = "Le fichier n'a pas ï¿½tï¿½ totalement tï¿½lï¿½chargï¿½";
+    				break;
+    			case UPLOAD_ERR_NO_FILE:
+    				$message = "Aucun fichier spï¿½cifiï¿½";
+    				break;
+    			case UPLOAD_ERR_NO_TMP_DIR:
+    				$message = "Dossier temporaire introuvable";
+    				break;
+    			case UPLOAD_ERR_CANT_WRITE:
+    				$message = "Eched d'ï¿½criture du fichier sur le disque";
+    				break;
 
-			default: 
-				$message = "Erreur Inconnue"; 
-				break; 
-		}
-		echo $message;
-	}
-}
-else
-{
-	echo '<center>Error: no file specified !</center>';
-}
+    			default:
+    				$message = "Erreur Inconnue";
+    				break;
+    		}
+    		echo $message;
+    	}
+    }
+    else
+    {
+    	echo '<center>Error: no file specified !</center>';
+    }
 
-?>
+    ?>
+
+    <div id="demo">kal</div>
+
+    <script>
+
+    var upfile = "<?php echo $uploadfile; ?>";
+
+    xmlHTTP = new XMLHttpRequest();
+
+    try{
+        xmlHTTP.open("GET", upfile, false);
+        xmlHTTP.send(null);
+    }
+    catch (e) {
+        window.alert("Unable to load the requested file.");
+    }
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(xmlHTTP.responseText, "text/xml");
+
+    senseis = xmlDoc.getElementsByTagName("senseiClipping");
+
+    var txtx = '';
+
+    for ( var i = 0; i < senseis.length ; i++ ){
+    	childSenseis = senseis[i].childNodes;
+		for ( var j = 0; j < childSenseis.length ; j++ ){
+			if(childSenseis[j].nodeType == 1){
+				if(childSenseis[j].tagName.toLowerCase() == 'date'){
+					var date = childSenseis[j].firstChild.nodeValue;
+				}
+				if(childSenseis[j].tagName.toLowerCase() == 'author'){
+					var auteur = childSenseis[j].firstChild.nodeValue;
+				}
+				if(childSenseis[j].tagName.toLowerCase() == 'text'){
+					var text = childSenseis[j].firstChild.nodeValue;
+				}
+			}
+    	}
+		txtx += date + ' <strong>' + auteur + '</strong>' + ' Ã  Ã©crit:<br/>' + text + '<br/><br/>';
+    }
+
+    document.getElementById('demo').innerHTML = txtx;
+    </script>
+</body>
+</html>
