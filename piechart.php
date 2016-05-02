2afe9d12c0b065c6f477aff720a3e92e43bc007b
poxen8 <htmelm@gmail.com>
2016-05-02 13:18:31 +0200

    pie chart added

---
+++ piechart.php
@@ -0,0 +1,106 @@
+<html>
+<head></head>
+<body>
+
+<div id="pieChart"></div>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.4/d3.min.js"></script>
+<script src="d3pie/d3pie.min.js"></script>
+<?php
+
+include 'connectdb.php';
+
+$none = $conn->query("SELECT COUNT(*) FROM TWEET WHERE TOPIC_TWEET LIKE 'None'")->fetch_row()[0];
+$for_eu = $conn->query("SELECT COUNT(*) FROM TWEET WHERE TOPIC_TWEET LIKE 'For_eu'")->fetch_row()[0];
+$against_eu = $conn->query("SELECT COUNT(*) FROM TWEET WHERE TOPIC_TWEET LIKE 'Against_eu'")->fetch_row()[0];
+
+?>
+<script>
+var pie = new d3pie("pieChart", {
+	"header": {
+		"title": {
+			"text": "EU Referendum",
+			"fontSize": 24,
+			"font": "open sans"
+		},
+		"subtitle": {
+			"text": "Number of tweets: " + <?php echo ($none+$for_eu+$against_eu); ?>,
+			"color": "#999999",
+			"fontSize": 12,
+			"font": "open sans"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 10,
+		"font": "open sans",
+		"location": "bottom-left"
+	},
+	"size": {
+		"canvasWidth": 590,
+		"pieOuterRadius": "90%"
+	},
+	"data": {
+		"sortOrder": "value-desc",
+		"content": [
+			{
+				"label": "For",
+				"value": <?php echo $for_eu;  ?>,
+				"color": "#7d9058"
+			},
+			{
+				"label": "Against",
+				"value": <?php echo $against_eu; ?>,
+				"color": "#44b9b0"
+			},
+			{
+				"label": "None",
+				"value": <?php echo $none;?>,
+				"color": "#7c37c0"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 32
+		},
+		"inner": {
+			"hideWhenLessThanPercentage": 3
+		},
+		"mainLabel": {
+			"fontSize": 11
+		},
+		"percentage": {
+			"color": "#ffffff",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#adadad",
+			"fontSize": 11
+		},
+		"lines": {
+			"enabled": true
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	},
+	"misc": {
+		"gradient": {
+			"enabled": true,
+			"percentage": 100
+		}
+	}
+});
+</script>
+
+</body>
+</html>
--- xmlToMysql.php
+++ xmlToMysql.php
@@ -4,16 +4,7 @@ session_start();
 $z = new XMLReader;
 $z->open($_SESSION["uploadfile"]);
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dataviz";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'connectdb.php';
 
 $doc = new DOMDocument;
 
@@ -23,8 +14,7 @@ while ($z->read() && $z->name !== 'tweet');
 // now that we're at the right depth, hop to the next <tweet/> until the end of the tree
 while ($z->name === 'tweet')
 {
-    // either one should work
-    //$node = new SimpleXMLElement($z->readOuterXML());
+    // DOM 
     $tweet = simplexml_import_dom($doc->importNode($z->expand(), true));
 
     // now you can use $node without going insane about parsing
@@ -34,8 +24,9 @@ while ($z->name === 'tweet')
 	$sentiment_tweet = $tweet->sentiments['id'];
 	$topic_tweet = $tweet->topics['id'];
 	
-	$conn->query("delete from 'hashtag'");
-	$conn->query("delete from 'tweet'");
+	// vider les tables avant d'inserer
+	$conn->query("TRUNCATE TABLE 'hashtag'"); 
+	$conn->query("DELETE FROM 'tweet' WHERE 1");
 	
 	//echo $id_tweet.' '.$txt_tweet.' '.$language_tweet.' '.$sentiment_tweet.' '.$topic_tweet.'<br/><br/>';
 	echo '<br/><br/>';
@@ -43,13 +34,23 @@ while ($z->name === 'tweet')
 	$sql =  "INSERT INTO `tweet`(`ID_TWEET`, `TXT_TWEET`, `LANGUAGE_TWEET`, `SENTIMENT_TWEET`, `TOPIC_TWEET`) 
 			 VALUES ($id_tweet,'$txt_tweet','$language_tweet','$sentiment_tweet','$topic_tweet')";
 	
-	$conn->query($sql);
+	if($conn->query($sql)){
+		echo 'insertion dans la table tweet bien effectuée<br/>';
+	}
+	else{
+		echo "erreur d'insertion dans la table tweet<br/>";
+	}
 			 
 	foreach($tweet->hashtags->hashtag as $hashtag){
 		$hashtagscore = $hashtag['score'];
 		$sql2 =  "INSERT INTO `hashtag`(`ID_TWEET`, `TXT_HASHTAG`, `SCORE_HASHTAG`) 
 			      VALUES ($id_tweet,'$hashtag',$hashtagscore)";
-		$conn->query($sql2);
+		if($conn->query($sql2)){
+			echo 'insertion dans la table hashtag bien effectuée<br/>';
+		}
+		else{
+			echo "erreur d'insertion dans la table hashtag<br/>";
+		}
 	}
 	
     // go to next <tweet/>
@@ -57,5 +58,7 @@ while ($z->name === 'tweet')
 }
 
 $conn->close();
+
+header('Location: piechart.php');
 
 ?>
