89907c5ba0fa30fee07fb3dd5c9cb195e94a7ed6
poxen8 <htmelm@gmail.com>
2016-05-19 01:37:16 +0200

    final topic charts

--- mslinechart.php
+++ mslinechart.php
@@ -95,7 +95,6 @@ function sendMslData(dated,datef){
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		 console.log(xmlhttp.responseText);
          loadChart("datax.txt");
      }
 };
